2a04574dd26c74182e02de4c26da1da2f3f5716d
Mitch Perez <perez15@live.missouristate.edu>
2019-07-20 18:17:36 -0500

    Committing my changes

--- pythonEmails.py
+++ pythonEmails.py
@@ -2,14 +2,20 @@ import smtplib, ssl
 import getpass
 
 port = 465  # For SSL
+
+# retrieve recipient's e-mail and sender's pass
 senderEmail = "mjp970501@gmail.com"
-password = getpass.getpass(prompt="Please enter your email's password: ", stream=None)
+sendTo = "perez15@live.missouristate.edu"
 userMessage = input("Please type your e-mail that you wish to send: ")
+password = input("Please type your e-mail's password: ")
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
+try:
     server.login(senderEmail, password)
-    server.sendmail(senderEmail, "perez15@live.missouristate.edu", userMessage)
-    # TODO: testing testing
+    server.sendmail(senderEmail, sendTo, userMessage)
+    # TODO: Send email here
+except:
+    print("Error, e-mail could not be sent. Please check the e-mail/pass you entered")
